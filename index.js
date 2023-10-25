287ad545aecb181f9e9e77b9da53db3733cd19cd
othmaneelbelghyty <othmane.elbelghyty@gmail.com>
2023-10-25 09:57:50 +0100

    friste commit

---
+++ index.js
@@ -0,0 +1,21 @@
+let result = 0;
+
+let arr1 = [3, 1, 7, 9];
+
+let arr2 = [2, 4, 1, 9, 3];
+
+for (let i = 0; i < arr1.length; i++) {
+  console.log(j, arr1[i]);
+  for (let j = 0; j < arr2.length; j++) {
+    if ((arr1[i] = 0 == arr2[j])) {
+    }
+  }
+}
+
+for (let k = 0; k < arr2.length; k++) {
+  for (let z = 0; z < arr1.length; z++) {
+    if (arr2[k] == arr1[z]) {
+      console.log(arr2[k]);
+    }
+  }
+}
